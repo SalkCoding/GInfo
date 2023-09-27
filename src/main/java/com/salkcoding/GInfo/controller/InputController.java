edcb4ae6da513d867e567b754e737bb772c2cf58
Sangbeom Jo <josangbeom1104@gmail.com>
2023-09-27 22:21:10 +0900

    PostMapping added, and Now result json string will be beautified by Gson for printing on HTML file. Plus, remove query parameter in endpoint url

--- src/main/java/com/salkcoding/GInfo/controller/InputController.java
+++ src/main/java/com/salkcoding/GInfo/controller/InputController.java
@@ -16,7 +16,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 
 @Slf4j
